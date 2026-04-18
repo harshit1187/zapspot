ab06093f790158a4559f399e5abd56e5b39f145e
Ajinkya Jadhav <ajinkyajadhav.email@gmail.com>
2026-04-19 04:32:50 +0530

    small bug

--- server/server.js
+++ server/server.js
@@ -20,7 +20,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    process.env.CLIENT_URL
+  ].filter(Boolean),
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
